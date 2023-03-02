b7e33cc39e3b01578f491f94e2a86c58e4b1094b
abelsoler <webabel@protonmail.com>
2023-03-02 19:56:53 +0100

    app.js modificado

--- js/app.js
+++ js/app.js
@@ -19,6 +19,59 @@ for(var x = 0; x <links.length;x++){
         menu_visible = false;
     }
 }
+/************ ANIMACIÓN FOOTER ************/
+// function([string1, string2],target id,[color1,color2])    
+consoleText(['Abel Soler 2023 ©', '¡ Gracias por venir !', '¿ Contactamos ?'], 'text',['#18b6a9','white','IndianRed']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+};
+
 
 
 /************ ESTRUCTURA APARICIÓN LOGOS ************/
