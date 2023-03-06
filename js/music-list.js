3ec09e02a96eb167a5681290378a606fdf910ca7
abelsoler <webabel@protonmail.com>
2023-03-06 17:15:52 +0100

    reproductor añadido

---
+++ js/music-list.js
@@ -0,0 +1,57 @@
+// Para añadir más canciones, simplemente copia el siguiente código y pégalo dentro del array
+
+// {
+// name: "Aquí va el nombre de la canción",
+// artist: "Aquí va el nombre del artista",
+// img: "nombre de la imagen aquí - recuerda que la imagen debe estar en formato .jpg y estar dentro de la carpeta 'images' en la carpeta de este proyecto",
+// src: "nombre de la canción aquí - recuerda que la canción debe estar en formato .mp3 y estar dentro de la carpeta 'songs' en la carpeta de este proyecto"
+// }
+
+// pégalos dentro del array tantas veces como quieras para añadir más música y no necesitarás hacer nada más.
+
+let allMusic = [
+    {
+      name: "Spectrum",
+      artist: "Abel Soler",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Accessing my senses",
+      artist: "Abel Soler",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Discover beyond the horizon",
+      artist: "Abel Soler",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Green Led",
+      artist: "Abel Soler",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Blue Stars",
+      artist: "Abel Soler",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Shine my inner light",
+      artist: "Abel Soler",
+      img: "music-6",
+      src: "music-6"
+    },
+        // pégalo así y recuerda dar una coma después del final de este corchete }
+        // {
+        //   nombre: "Aquí va el nombre de la canción",
+        //   artista: "Aquí va el nombre del artista",
+        //   img: "aquí va el nombre de la imagen - recuerda que debe estar en formato .jpg y estar dentro de la carpeta de imágenes de este proyecto",
+        //   src: "aquí va el nombre de la canción - recuerda que debe estar en formato .mp3 y estar dentro de la carpeta de canciones de este proyecto"
+        // }
+
+  ];
